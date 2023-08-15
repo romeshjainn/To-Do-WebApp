568da0b4e0547f75c9875234faf3c993d92b47d6
romeshjainn <romeshjainofficial@gmail.com>
2023-08-15 08:03:59 +0530

    container, and one card created

---
+++ script.js
@@ -0,0 +1,14 @@
+let container = document.createElement("div");
+container.className = "container";
+document.body.appendChild(container);
+
+let card = document.createElement("div");
+card.className = "card";
+
+
+
+if (1 == 1) {
+    container.appendChild(card)
+    card.style.border = "2px solid red";
+}
+
