3c0bb53d644bda1b954b95781702aa377debac6a
romeshjainn <romeshjainofficial@gmail.com>
2023-08-15 08:51:53 +0530

    create card btn added

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
+// Creating Elements 
+
+let header = document.createElement("div");
+let createBtn = document.createElement("button");
+let deleteBtn = document.createElement("button");
 let container = document.createElement("div");
-container.className = "container";
 let card = document.createElement("div");
-card.className = "card";
 let inputBox = document.createElement("input");
-inputBox.className = "inputBox";
-inputBox.placeholder = "Enter The TItle"
 let discriptionBox = document.createElement("input");
+
+// Giving CLasses to Elements 
+
+header.className = "header";
+createBtn.className = "createBtn";
+createBtn.innerText = "Create Card";
+deleteBtn.className = "deleteBtn";
+deleteBtn.innerText = "Delete Card";
+container.className = "container";
+card.className = "card";
+inputBox.className = "inputBox";
 discriptionBox.className = "discriptionBox";
-discriptionBox.placeholder = "Enter The Discription"
+inputBox.placeholder = "Enter The TItle";
+discriptionBox.placeholder = "Enter The Discription";
+
+// Appending ELements 
+
+document.body.appendChild(header);
+header.appendChild(createBtn);
+header.appendChild(deleteBtn);
 document.body.appendChild(container);
-container.appendChild(card);
 card.append(inputBox, discriptionBox);
 
+createBtn.onclick = function () {
+    createBtn = [];
+    for (let i = 0; i < createBtn.length + 1; i++){
+        container.appendChild(card.cloneNode(true));
+        card.classList.add("cardData");
+    }
+}
+let allCards = document.getElementsByClassName("cardData");
 
-
-
-
+for (let i = 0; i < allCards.length; i++){
+    let data = allCards[i].value; 
+    allCards[i].innerText = data;
+}
+deleteBtn.onclick = function () {
+    
+}
